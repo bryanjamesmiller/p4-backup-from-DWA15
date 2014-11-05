476f96ca04c54a0552a5c29f3ee1c4a95fbd9e1a
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-11-04 23:24:50 -0600

    yet more blade :)

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -10,17 +10,7 @@ Degree Tracker PET
 
 
 @section('top')
-    <div id="heading">
-        <h1 id="wheatImage">
-            <img src="images/wheat logo.PNG" alt="wheat logo">
-        </h1>
-        <caption>
-            <h1 id="studentName">
-                <label for="name" id="name">Student Name: </label>
-                <input type="text" class="textboxes" name="name" id="name">
-            </h1>
-        </caption>
-    </div>
+    <p> @include('banner_header')</p>
 @stop
 
 @section('middle')
