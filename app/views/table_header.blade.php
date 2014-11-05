421c0ae8f050d5633d873a9349e6d04043d2fe34
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-11-04 23:14:17 -0600

    more blade

---
+++ app/views/table_header.blade.php
@@ -0,0 +1,22 @@
+<table id="myTable" class="tablesorter">
+        <thead>
+        <tr id="tableHeaders">
+            <th>Course Number</th>
+            <th>Course Delivery</th>
+            <th>CRN#</th>
+            <th>Section</th>
+            <th>Tuition</th>
+            <th>Course Title</th>
+            <th colspan="5">Course Attributes</th>
+            <th>Semester</th>
+            <th>Day(s)</th>
+            <th>Time(s)</th>
+            <th>Year</th>
+            <th>Professor(s)</th>
+            <th>Status</th>
+            <th>Grade</th>
+            <th>Grade Points</th>
+            <th>Transfer Credits</th>
+            <th>HES Credits</th>
+        </tr>
+        </thead>
