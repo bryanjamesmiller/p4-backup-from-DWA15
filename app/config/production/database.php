2d5c3928b90b7fa8bef274c64de703892b3abb79
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-11-06 12:15:42 -0600

    adding db to prod

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'pets',
+            'username'  => 'root',
+            'password'  => 'hH6NKiy9cPe',
+        ),
+
+    ),
+
+);
