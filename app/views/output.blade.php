6f5932f6183519e4ee391421243da41851173d69
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-11-07 17:07:21 -0600

    t

--- app/views/output.blade.php
+++ app/views/output.blade.php
@@ -28,30 +28,30 @@
 
         # Typically we'd pass $books to a View, but for quick and dirty demonstration, let's just output here...
         foreach($allCourses as $oneCourse) {
-            echo '<td>' . $oneCourse->course_name. '<td>';
-            
-            echo $oneCourse->course_delivery.' ';
-            echo $oneCourse->crn_number.' ';
-            echo $oneCourse->section . ' ';
-                        echo $oneCourse->tuition . ' ';
-                        echo $oneCourse->course_title . ' ';
-                        echo $oneCourse->course_attributes_1 . ' ';
-                        echo $oneCourse->course_attributes_2 . ' ';
-                        echo $oneCourse->course_attributes_3 . ' ';
-                        echo $oneCourse->course_attributes_4 . ' ';
-                        echo $oneCourse->course_attributes_5 . ' ';
-                        echo $oneCourse->semester . ' ';
-                        echo $oneCourse->days . ' ';
-                        echo $oneCourse->times . ' ';
-                        echo $oneCourse->year . ' ';
-                        echo $oneCourse->professors . ' ';
-                        echo $oneCourse->status . ' ';
-                        echo $oneCourse->letter_grade . ' ';
-                        echo $oneCourse->grade_points . ' ';
-                        echo $oneCourse->transfer_credits . ' ';
-                        echo $oneCourse->hes_credits . ' ';
+               echo '<td id="course_number">' . $oneCourse->course_name. '</td>';
 
-            echo '<br>';
+                        echo '<td id="course_delivery" class="dropdowns">' .$oneCourse->course_delivery.' </td>';
+                        echo '<td id="crn_number">' .$oneCourse->crn_number.'</td> ';
+                        echo '<td id="section">' . $oneCourse->section . '</td> ';
+                                    echo '<td id="tuition">' .$oneCourse->tuition . '</td> ';
+                                    echo '<td id="course_title">' .$oneCourse->course_title . '</td> ';
+                                    echo '<td class="dropdowns">' .$oneCourse->course_attributes_1 . ' </td>';
+                                    echo '<td class="dropdowns">' .$oneCourse->course_attributes_2 . '</td> ';
+                                    echo '<td class="dropdowns">' .$oneCourse->course_attributes_3 . '</td> ';
+                                    echo '<td class="dropdowns">' .$oneCourse->course_attributes_4 . '</td> ';
+                                    echo '<td class="dropdowns">' .$oneCourse->course_attributes_5 . '</td> ';
+                                    echo  '<td class="dropdowns" id="semester">' .$oneCourse->semester . '</td> ';
+                                    echo  '<td id="days">' .$oneCourse->days . '</td> ';
+                                    echo  '<td id="times">' .$oneCourse->times . '</td> ';
+                                    echo  '<td id="year">' .$oneCourse->year . '</td> ';
+                                    echo  '<td id="professors">' .$oneCourse->professors . '</td> ';
+                                    echo  '<td class="dropdowns" id="status>' .$oneCourse->status . '</td> ';
+                                    echo '<td class="dropdowns" id="letter_grade">' .$oneCourse->letter_grade . '</td> ';
+                                    echo  '<class="dropdowns" td id="grade_points">' .$oneCourse->grade_points . '</td> ';
+                                    echo  '<td id="transfer_credits">' .$oneCourse->transfer_credits . '</td>';
+                                    echo '<td id="hes_credits">' . $oneCourse->hes_credits . '</td></tr> ';
+
+            
         }
     }
     else {
