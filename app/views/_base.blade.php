6b0017d2bd3c01611b06ef6899f9e1c49bfdca5d
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-11-21 21:34:40 -0600

    sign up and log in - need to make server db

--- app/views/_base.blade.php
+++ app/views/_base.blade.php
@@ -13,7 +13,13 @@
     <body>
     @if(Session::get('flash_message'))
     	<div class='flash-message'>{{ Session::get('flash_message') }}</div>
-    	@endif
+    @endif
+
+    @if(Auth::check())
+        <a href='/logout'>Log out {{ Auth::user()->email; }}</a>
+    @else
+        <a href='/signup'>Sign up</a> or <a href='/login'>Log in</a>
+    @endif
 
     @yield('top')
 
