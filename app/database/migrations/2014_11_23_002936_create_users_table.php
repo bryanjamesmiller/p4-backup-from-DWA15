e08a0625070d6361c36c73b020e5fa6883fdaa67
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-11-22 18:35:24 -0600

    db

--- app/database/migrations/2014_11_23_002936_create_users_table.php
+++ app/database/migrations/2014_11_23_002936_create_users_table.php
@@ -5,32 +5,33 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
         Schema::create('users', function($table) {
 
             $table->increments('id');
             $table->string('email')->unique();
+            $table->string('student_name');
             $table->string('remember_token',100);
             $table->string('password');
             $table->timestamps();
 
         });
-	}
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
         Schema::drop('users');
-	}
+    }
 
 }
