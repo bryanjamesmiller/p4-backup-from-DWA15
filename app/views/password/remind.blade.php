9fd8499f1de4f8ab84d683f0035cb31f7b43e5fe
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-11-26 16:20:14 -0600

    css

--- app/views/password/remind.blade.php
+++ app/views/password/remind.blade.php
@@ -1,6 +1,8 @@
 <div class="font_wrapper_sign_in_log_in">
+Would you like to reset your password?<br>
+Please enter your email address:
 <form action="{{ action('RemindersController@postRemind') }}" method="POST">
-    <input type="email" name="email">
+    <input type="email" name="email"><br>
     <input type="submit" value="Email Link to Create New Password">
 </form>
 </div>
