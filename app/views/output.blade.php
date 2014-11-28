2ea51c3a1d18ff80b1726166b7c11e784f7345aa
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-11-28 17:32:44 -0600

    t

--- app/views/output.blade.php
+++ app/views/output.blade.php
@@ -59,7 +59,7 @@ echo '<tr>
 <tr class="data_row">
     <td class = "edit_or_delete_rows" colspan="22">
              <span>
-                    <a class="edit_or_delete_text" href="/edit/' ?> {{$oneCourse->id}}<?php echo '" > Edit course information</a>
+                    <a class="edit_or_delete_text" href="/course_edit/' ?> {{$oneCourse->id}}<?php echo '" > Edit course information</a>
              </span>
              <span>
                     <a class="edit_or_delete_text" href="/delete/' ?> {{$oneCourse->id}}<?php echo '" > Delete course entry </a>
