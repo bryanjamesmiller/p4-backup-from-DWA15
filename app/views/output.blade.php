64f68080c130065a4bb609b5c7c00a420b29e009
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-11-29 17:29:05 -0600

    t

--- app/views/output.blade.php
+++ app/views/output.blade.php
@@ -15,15 +15,28 @@
 {
 echo '
 <tr>
+    <th class="course_title_header" colspan="2">Course Title</th>
     <th class="course_attributes_header" colspan="5">Course Attributes</th>
+              <th class="semester_header">Semester</th>
+              <th class="days_header">Day(s)</th>
+              <th class="times_header">Time(s)</th>
+              <th class="year_header">Year</th>
+              <th class="professors_header">Professor(s)</th>
 </tr>
 
 <tr>
+<td class="course_title_box">' ?>  {{{$oneCourse->course_title  }}}<?php echo '</td>
     <td class="course_attributes_box">' ?>  {{{$oneCourse->course_attributes_1  }}}<?php echo ' </td>
     <td class="course_attributes_box">' ?>  {{{$oneCourse->course_attributes_2  }}}<?php echo '</td>
     <td class="course_attributes_box">' ?>  {{{$oneCourse->course_attributes_3  }}}<?php echo '</td>
     <td class="course_attributes_box">' ?>  {{{$oneCourse->course_attributes_4  }}}<?php echo '</td>
     <td class="course_attributes_box">' ?>  {{{$oneCourse->course_attributes_5  }}}<?php echo '</td>
+<td class="semester_box">' ?>  {{{$oneCourse->semester  }}}<?php echo '</td>
+<td class="days_box">' ?>  {{{$oneCourse->days  }}}<?php echo '</td>
+<td class="times_box">' ?>  {{{$oneCourse->times  }}}<?php echo '</td>
+<td class="year_box">' ?>  {{{$oneCourse->year  }}}<?php echo '</td>
+<td class="professors_box">' ?>  {{{$oneCourse->professors  }}}<?php echo '</td>
+
 </tr>
 
 
@@ -33,12 +46,6 @@ echo '
               <th class="crn_number_header">CRN#</th>
               <th  class="section_header">Section</th>
               <th class="tuition_header">Tuition</th>
-              <th class="course_title_header">Course Title</th>
-              <th class="semester_header">Semester</th>
-              <th class="days_header">Day(s)</th>
-              <th class="times_header">Time(s)</th>
-              <th class="year_header">Year</th>
-              <th class="professors_header">Professor(s)</th>
               <th class="status_header">Status*</th>
               <th class="letter_grade_header">Grade</th>
               <th class="grade_points_header">Grade Points**</th>
@@ -51,12 +58,6 @@ echo '
 <td class="crn_number_box">' ?>  {{{$oneCourse->crn_number }}}<?php echo '</td>
 <td class="section_box">' ?>  {{{ $oneCourse->section  }}}<?php echo '</td>
 <td class="tuition_box">' ?>  {{{$oneCourse->tuition  }}}<?php echo '</td>
-<td class="course_title_box">' ?>  {{{$oneCourse->course_title  }}}<?php echo '</td>
-<td class="semester_box">' ?>  {{{$oneCourse->semester  }}}<?php echo '</td>
-<td class="days_box">' ?>  {{{$oneCourse->days  }}}<?php echo '</td>
-<td class="times_box">' ?>  {{{$oneCourse->times  }}}<?php echo '</td>
-<td class="year_box">' ?>  {{{$oneCourse->year  }}}<?php echo '</td>
-<td class="professors_box">' ?>  {{{$oneCourse->professors  }}}<?php echo '</td>
 <td class="status_box">' ?>  {{{$oneCourse->status  }}}<?php echo '</td>
 <td class="letter_grade_box">' ?>  {{{$oneCourse->letter_grade  }}}<?php echo '</td>
 <td class="grade_points_box">' ?>  {{{$oneCourse->grade_points  }}}<?php echo '</td>
