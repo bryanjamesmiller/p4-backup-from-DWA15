6191994ebf1cf80ab15363ccfeb06eee684d0c15
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-11-29 21:45:34 -0600

    t

--- app/views/forms_input.blade.php
+++ app/views/forms_input.blade.php
@@ -21,41 +21,37 @@ else
    <table>
         <thead>
         <tr>
-            <th class="course_title_header">Course Title</th>
+            <th class="course_title_header" colspan="2">Course Title</th>
             <th class="days_header">Day(s)</th>
             <th class="times_header">Time(s)</th>
             <th class="year_header">Year</th>
             <th class="professors_header">Professor(s)</th>
+              <th class="semester_header">Semester</th>
+                      <th class="course_delivery_header">Course Delivery</th>
+                          <th class="crn_number_header">CRN#</th>
+
         </tr>
         </thead>
 
     <tr>
-    <td class="course_title_box"> {{Form::text('course_title', '', array('class' => 'course_title'))}}</td>
+    <td class="course_title_box" colspan="2"> {{Form::text('course_title', '', array('class' => 'course_title'))}}</td>
 
     <td class="days_box"> {{Form::text('days', '', array('class' => 'days'))}}</td>
     <td class="times_box"> {{Form::text('times', '', array('class' => 'times'))}}</td>
     <td class="year_box"> {{Form::text('year', '', array('class' => 'year'))}}</td>
     <td class="professors_box"> {{Form::text('professors', '', array('class' => 'professors'))}}</td>
-
-</tr>
-<tr>
- <th class="course_number_header">Course Number</th>
-  <th class="semester_header">Semester</th>
-            <th class="course_delivery_header">Course Delivery</th>
-            <th class="crn_number_header">CRN#</th>
-            <th  class="section_header">Section</th>
-            <th class="tuition_header">Tuition</th>
-            <!-- <th class="course_attributes_header" colspan="5">Course Attributes</th> -->
-
-
-            <th class="status_header">Status</th>
-            <th class="letter_grade_header">Grade</th>
-            <!--<th class="grade_points_header">Grade Points</th> -->
-            <th class="transfer_credits_header">Transfer Credits</th>
-            <th class="hes_credits_header">HES Credits</th>
-
-</tr>
-<tr>
+<td class="semester_box">
+        <select name="semester" class="semester">
+            <optgroup>
+                <option>Fall</option>
+                <option>January</option>
+                <option>Spring</option>
+                <option>June</option>
+                <option>Summer</option>
+                <option>Other</option>
+            </optgroup>
+        </select>
+    </td>
 <td class="course_number_box"> {{Form::text('course_number', '', array('class' => 'course_number'))}}</td>
     <td class="course_delivery_box">
         <select name="course_delivery" class="course_delivery">
@@ -70,20 +66,25 @@ else
         </select>
     </td>
     <td class="crn_number_box"> {{Form::text('crn_number', '', array('class' => 'crn_number'))}}</td>
+</tr>
+<tr>
+ <th class="course_number_header">Course Number</th>
+
+            <th  class="section_header">Section</th>
+            <th class="tuition_header">Tuition</th>
+            <!-- <th class="course_attributes_header" colspan="5">Course Attributes</th> -->
+
+
+            <th class="status_header">Status</th>
+            <th class="letter_grade_header">Grade</th>
+            <!--<th class="grade_points_header">Grade Points</th> -->
+            <th class="transfer_credits_header">Transfer Credits</th>
+            <th class="hes_credits_header">HES Credits</th>
+
+</tr>
+<tr>
     <td class="section_box"> {{Form::text('section', '', array('class' => 'section'))}}</td>
     <td class="tuition_box"> {{Form::text('tuition', '', array('class' => 'tuition'))}}</td>
-    <td class="semester_box">
-        <select name="semester" class="semester">
-            <optgroup>
-                <option>Fall</option>
-                <option>January</option>
-                <option>Spring</option>
-                <option>June</option>
-                <option>Summer</option>
-                <option>Other</option>
-            </optgroup>
-        </select>
-    </td>
     <td class="status_box">
         <select name="status" class="status">
             <optgroup>
