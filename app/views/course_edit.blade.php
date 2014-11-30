345b5697c3c0f895549bf98f6b322367a7380e27
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-11-29 18:50:46 -0600

    t

--- app/views/course_edit.blade.php
+++ app/views/course_edit.blade.php
@@ -14,50 +14,58 @@ Edit Course Information
 @section('middle')
 <div class="font_wrapper">
 <table class="myTable">
-    <thead>
-    <tr class="tableHeaders">
-        <th class="course_number_header">Course Number</th>
-                    <th class="course_delivery_header">Course Delivery</th>
-                    <th class="crn_number_header">CRN#</th>
-                    <th class="section_header">Section</th>
-                    <th class="tuition_header">Tuition</th>
-                    <th class="course_title_header">Course Title</th>
-                    <th class="course_attributes_header" colspan="5">Course Attributes</th>
-                    <th class="semester_header">Semester</th>
-                    <th class="days_header">Day(s)</th>
-                    <th class="time_header">Time(s)</th>
-                    <th class="year_header">Year</th>
-                    <th class="professors_header">Professor(s)</th>
-                    <th class="status_header">Status*</th>
-                    <th class="letter_grade_header">Grade**</th>
-                    <th class="grade_points_header">Grade Points**</th>
-                    <th class="transfer_credits_header">Transfer Credits</th>
-                    <th class="hes_credits_header">HES Credits</th>
+<tr>
+    <th class="course_title_header" colspan="2">Course Title</th>
+    <th class="course_attributes_header" colspan="5">Course Attributes</th>
+
+              <th class="days_header">Day(s)</th>
+              <th class="times_header">Time(s)</th>
+              <th class="year_header">Year</th>
+              <th class="professors_header">Professor(s)</th>
+</tr>
+
+<tr>
+<td class="course_title_box" colspan="2">  {{{$course->course_title  }}}</td>
+    <td class="course_attributes_box"> {{{$course->course_attributes_1  }}} </td>
+    <td class="course_attributes_box">{{{$course->course_attributes_2  }}}</td>
+    <td class="course_attributes_box"> {{{$course->course_attributes_3  }}}</td>
+    <td class="course_attributes_box"> {{{$course->course_attributes_4  }}}</td>
+    <td class="course_attributes_box">  {{{$course->course_attributes_5  }}}</td>
+
+<td class="days_box">  {{{$course->days  }}}</td>
+<td class="times_box">  {{{$course->times  }}}</td>
+<td class="year_box">  {{{$course->year  }}}</td>
+<td class="professors_box">  {{{$course->professors  }}}</td>
+
+</tr>
+
+
+<tr>
+<th class="semester_header">Semester</th>
+              <th class="course_number_header">Course Number</th>
+              <th class="course_delivery_header">Course Delivery</th>
+              <th class="crn_number_header">CRN#</th>
+              <th  class="section_header">Section</th>
+              <th class="tuition_header">Tuition</th>
+              <th class="status_header">Status*</th>
+              <th class="letter_grade_header">Grade</th>
+              <th class="grade_points_header">Grade Points**</th>
+              <th class="transfer_credits_header">Transfer Credits**</th>
+              <th class="hes_credits_header">HES Credits**</th>
     </tr>
-    </thead>
     <tr>
-        <td class="course_number_box">{{{$course->course_number}}}</td>
-        <td class="course_delivery_box">  {{{$course->course_delivery }}} </td>
-        <td class="crn_number_box">  {{{$course->crn_number }}}</td>
-        <td class="section_box">  {{{ $course->section  }}}</td>
-        <td class="tuition_box">  {{{$course->tuition  }}}</td>
-        <td class="course_title_box">  {{{$course->course_title  }}}</td>
-        <td class="course_attributes_box">  {{{$course->course_attributes_1  }}} </td>
-        <td class="course_attributes_box">  {{{$course->course_attributes_2  }}}</td>
-        <td class="course_attributes_box">  {{{$course->course_attributes_3  }}}</td>
-        <td class="course_attributes_box">  {{{$course->course_attributes_4  }}}</td>
-        <td class="course_attributes_box">  {{{$course->course_attributes_5  }}}</td>
-        <td class="semester_box">  {{{$course->semester  }}}</td>
-        <td class="days_box">  {{{$course->days  }}}</td>
-        <td class="times_box">  {{{$course->times  }}}</td>
-        <td class="year_box">  {{{$course->year  }}}</td>
-        <td class="professors_box">  {{{$course->professors  }}}</td>
-        <td class="status_box">  {{{$course->status  }}}</td>
-        <td class="letter_grade_box">  {{{$course->letter_grade  }}}</td>
-       <!-- <td class="grade_points_box">  {{{$course->grade_points  }}}</td>-->
-        <td class="transfer_credits_box">  {{{$course->transfer_credits  }}}</td>
-        <td class="hes_credits_box">  {{{ $course->hes_credits  }}}</td>
-    </tr>
+<td class="semester_box">  {{{$course->semester  }}}</td>
+<td class="course_number_box"> {{{$course->course_number}}} </td>
+<td class="course_delivery_box">  {{{$course->course_delivery }}} </td>
+<td class="crn_number_box">  {{{$course->crn_number }}}</td>
+<td class="section_box">  {{{ $course->section  }}}</td>
+<td class="tuition_box">  {{{$course->tuition  }}}</td>
+<td class="status_box">  {{{$course->status  }}}</td>
+<td class="letter_grade_box">  {{{$course->letter_grade  }}}</td>
+<td class="grade_points_box">  {{{$course->grade_points  }}}</td>
+<td class="transfer_credits_box">  {{{$course->transfer_credits  }}}</td>
+<td class="hes_credits_box">  {{{ $course->hes_credits  }}}</td>
+</tr>
 </table>
 
 
