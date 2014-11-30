9eef77a2965ec3c940eadb92174aa92a310da264
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-11-29 19:38:42 -0600

    t

--- app/views/forms_input.blade.php
+++ app/views/forms_input.blade.php
@@ -3,27 +3,23 @@
    <table>
         <thead>
         <tr>
+            <th class="course_title_header">Course Title</th>
             <th class="course_number_header">Course Number</th>
             <th class="course_delivery_header">Course Delivery</th>
             <th class="crn_number_header">CRN#</th>
             <th  class="section_header">Section</th>
             <th class="tuition_header">Tuition</th>
-            <th class="course_title_header">Course Title</th>
             <!-- <th class="course_attributes_header" colspan="5">Course Attributes</th> -->
             <th class="semester_header">Semester</th>
             <th class="days_header">Day(s)</th>
             <th class="times_header">Time(s)</th>
             <th class="year_header">Year</th>
             <th class="professors_header">Professor(s)</th>
-            <th class="status_header">Status</th>
-            <th class="letter_grade_header">Grade</th>
-            <!--<th class="grade_points_header">Grade Points</th> -->
-            <th class="transfer_credits_header">Transfer Credits</th>
-            <th class="hes_credits_header">HES Credits</th>
         </tr>
         </thead>
 
     <tr>
+    <td class="course_title_box"> {{Form::text('course_title', '', array('class' => 'course_title'))}}</td>
     <td class="course_number_box"> {{Form::text('course_number', '', array('class' => 'course_number'))}}</td>
     <td class="course_delivery_box">
         <select name="course_delivery" class="course_delivery">
@@ -40,7 +36,7 @@
     <td class="crn_number_box"> {{Form::text('crn_number', '', array('class' => 'crn_number'))}}</td>
     <td class="section_box"> {{Form::text('section', '', array('class' => 'section'))}}</td>
     <td class="tuition_box"> {{Form::text('tuition', '', array('class' => 'tuition'))}}</td>
-    <td class="course_title_box"> {{Form::text('course_title', '', array('class' => 'course_title'))}}</td>
+
 Check the relevant course attributes:<br>
 <?php
 $account = Account::search(Auth::user()->email);
@@ -74,6 +70,17 @@ else
     <td class="times_box"> {{Form::text('times', '', array('class' => 'times'))}}</td>
     <td class="year_box"> {{Form::text('year', '', array('class' => 'year'))}}</td>
     <td class="professors_box"> {{Form::text('professors', '', array('class' => 'professors'))}}</td>
+
+</tr>
+<tr>
+            <th class="status_header">Status</th>
+            <th class="letter_grade_header">Grade</th>
+            <!--<th class="grade_points_header">Grade Points</th> -->
+            <th class="transfer_credits_header">Transfer Credits</th>
+            <th class="hes_credits_header">HES Credits</th>
+
+</tr>
+<tr>
     <td class="status_box">
         <select name="status" class="status">
             <optgroup>
@@ -110,6 +117,7 @@ else
     <td class="transfer_credits_box"> {{Form::text('transfer_credits', '', array('class' => 'transfer_credits'))}}</td>
     <td class="hes_credits_box"> {{Form::text('hes_credits', '', array('class' => 'hes_credits'))}}</td>
 </tr>
+
 </table>
 <div class="buttons">{{ Form::submit('Add Course')}}</div>
 {{ Form::close() }}
