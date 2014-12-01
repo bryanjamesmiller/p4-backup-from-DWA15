f731063796058df00ad6f6132b0390ea19f1febc
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-12-01 16:21:19 -0600

    t

--- app/views/course_attributes_checkbox_alb.blade.php
+++ app/views/course_attributes_checkbox_alb.blade.php
@@ -1,11 +1,11 @@
   <div>  <span class="checkboxes_alb">{{ Form::label('sciences', 'Sciences') }}{{ Form::checkbox('sciences', 'y') }}</span>
-  <span class="checkboxes_alb">{{ Form::label('social_sciences', 'Social Sciences') }}{{ Form::checkbox('social_sciences', 'y') }}</span>
+  <span class="checkboxes_alb">{{ Form::label('social_sciences', 'Social Sciences') }}{{ Form::checkbox('social_sciences', 'y') }}</span></div>
  <span class="checkboxes_alb"> {{ Form::label('humanities', 'Humanities') }}{{ Form::checkbox('humanities', 'y') }}</span>
- <span class="checkboxes_alb"> {{ Form::label('expository_writing', 'Expository Writing') }}{{ Form::checkbox('expository_writing', 'y') }}</span></div>
-  <div><span class="checkboxes_alb">{{ Form::label('writing_intensive', 'Writing Intensive') }}{{ Form::checkbox('writing_intensive', 'y') }}</span>
- <span class="checkboxes_alb"> {{ Form::label('quantitative_reasoning', 'Quantitative Reasoning') }}{{ Form::checkbox('quantitative_reasoning', 'y') }}</span>
-  <span class="checkboxes_alb">{{ Form::label('moral_reasoning', 'Moral Reasoning') }}{{ Form::checkbox('moral_reasoning', 'y') }}</span>
- <span class="checkboxes_alb"> {{ Form::label('foreign_language', 'Foreign Language') }}{{ Form::checkbox('foreign_language', 'y') }}</span></div>
+ <div><span class="checkboxes_alb"> {{ Form::label('expository_writing', 'Expository Writing') }}{{ Form::checkbox('expository_writing', 'y') }}</span>
+  <span class="checkboxes_alb">{{ Form::label('writing_intensive', 'Writing Intensive') }}{{ Form::checkbox('writing_intensive', 'y') }}</span>
+   <span class="checkboxes_alb"> {{ Form::label('foreign_language', 'Foreign Language') }}{{ Form::checkbox('foreign_language', 'y') }}</span></div>
+<div><span class="checkboxes_alb"> {{ Form::label('quantitative_reasoning', 'Quantitative Reasoning') }}{{ Form::checkbox('quantitative_reasoning', 'y') }}</span>
+  <span class="checkboxes_alb">{{ Form::label('moral_reasoning', 'Moral Reasoning') }}{{ Form::checkbox('moral_reasoning', 'y') }}</span></div>
   <div><span class="checkboxes_alb">{{ Form::label('harvard_instructor', 'Harvard Instructor') }}{{ Form::checkbox('harvard_instructor', 'y') }}</span>
   <span class="checkboxes_alb">{{ Form::label('upper_level_course', 'Upper Level Course') }}{{ Form::checkbox('upper_level_course', 'y') }}</span>
  <div> <span class="checkboxes_alb">{{ Form::label('residency', 'Residency') }}{{ Form::checkbox('residency', 'y') }}</span>
