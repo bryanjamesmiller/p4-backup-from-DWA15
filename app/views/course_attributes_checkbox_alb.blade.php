ada42063f58a40f482b0ea1866120012b891a4f4
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-12-01 15:56:33 -0600

    t

--- app/views/course_attributes_checkbox_alb.blade.php
+++ app/views/course_attributes_checkbox_alb.blade.php
@@ -1,5 +1,5 @@
-  <div>  <span class="checkboxes_alb">{{ Form::label('sciences', 'Sciences') }}{{ Form::checkbox(''sciences', 'y') }}</span>
-  <span class="checkboxes_alb">{{ Form::label('social_sciences', 'Social Sciences') }}{{ Form::checkbox(''social_sciences', 'y') }}</span>
+  <div>  <span class="checkboxes_alb">{{ Form::label('sciences', 'Sciences') }}{{ Form::checkbox('sciences', 'y') }}</span>
+  <span class="checkboxes_alb">{{ Form::label('social_sciences', 'Social Sciences') }}{{ Form::checkbox(social_sciences', 'y') }}</span>
  <span class="checkboxes_alb"> {{ Form::label('humanities', 'Humanities') }}{{ Form::checkbox('humanities', 'y') }}</span>
  <span class="checkboxes_alb"> {{ Form::label('expository_writing', 'Expository Writing') }}{{ Form::checkbox('expository_writing', 'y') }}</span></div>
   <div><span class="checkboxes_alb">{{ Form::label('writing_intensive', 'Writing Intensive') }}{{ Form::checkbox('writing_intensive', 'y') }}</span>
@@ -10,4 +10,4 @@
   <span class="checkboxes_alb">{{ Form::label('upper_level_course', 'Upper Level Course') }}{{ Form::checkbox('upper_level_course', 'y') }}</span>
   <span class="checkboxes_alb">{{ Form::label('residency', 'Residency') }}{{ Form::checkbox('residency', 'y') }}</span>
  <span class="checkboxes_alb"> {{ Form::label('field_of_study', 'Field Of Study') }}{{ Form::checkbox('field_of_study', 'y') }}</span></div>
- <div><span class="checkboxes_alb"> {{ Form::label('minor', Minor'') }}{{ Form::checkbox('minor', 'y') }}</span></div>
+ <div><span class="checkboxes_alb"> {{ Form::label('minor', Minor') }}{{ Form::checkbox('minor', 'y') }}</span></div>
