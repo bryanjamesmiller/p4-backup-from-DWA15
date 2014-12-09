1aa9ee404fce4acfed3cf8f1b3c76d7cf07ec1a3
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-12-09 16:43:32 -0600

    t

--- app/views/output.blade.php
+++ app/views/output.blade.php
@@ -21,64 +21,61 @@ echo '
 <tr>
     <th class="course_title_header" colspan="2">Course Title</th>
     <th class="course_attributes_header" colspan="5">Course Attributes</th>
-
-              <th class="days_header">Day(s)</th>
-              <th class="times_header">Time(s)</th>
-              <th class="year_header">Year</th>
-              <th class="professors_header">Professor(s)</th>
+    <th class="days_header">Day(s)</th>
+    <th class="times_header">Time(s)</th>
+    <th class="year_header">Year</th>
+    <th class="professors_header">Professor(s)</th>
 </tr>
 
 <tr>
-<td class="course_title_box" colspan="2">' ?>  {{{$oneCourse->course_title  }}}<?php echo '</td>
+    <td class="course_title_box" colspan="2">' ?>  {{{$oneCourse->course_title  }}}<?php echo '</td>
     <td class="course_attributes_box">' ?>  {{{$oneCourse->course_attributes_1  }}}<?php echo ' </td>
     <td class="course_attributes_box">' ?>  {{{$oneCourse->course_attributes_2  }}}<?php echo '</td>
     <td class="course_attributes_box">' ?>  {{{$oneCourse->course_attributes_3  }}}<?php echo '</td>
     <td class="course_attributes_box">' ?>  {{{$oneCourse->course_attributes_4  }}}<?php echo '</td>
     <td class="course_attributes_box">' ?>  {{{$oneCourse->course_attributes_5  }}}<?php echo '</td>
-
-<td class="days_box">' ?>  {{{$oneCourse->days  }}}<?php echo '</td>
-<td class="times_box">' ?>  {{{$oneCourse->times  }}}<?php echo '</td>
-<td class="year_box">' ?>  {{{$oneCourse->year  }}}<?php echo '</td>
-<td class="professors_box">' ?>  {{{$oneCourse->professors  }}}<?php echo '</td>
-
+    <td class="days_box">' ?>  {{{$oneCourse->days  }}}<?php echo '</td>
+    <td class="times_box">' ?>  {{{$oneCourse->times  }}}<?php echo '</td>
+    <td class="year_box">' ?>  {{{$oneCourse->year  }}}<?php echo '</td>
+    <td class="professors_box">' ?>  {{{$oneCourse->professors  }}}<?php echo '</td>
 </tr>
 
+<tr>
+    <th class="semester_header">Semester</th>
+    <th class="course_number_header">Course Number</th>
+    <th class="course_delivery_header">Course Delivery</th>
+    <th class="crn_number_header">CRN#</th>
+    <th  class="section_header">Section</th>
+    <th class="tuition_header">Tuition</th>
+    <th class="status_header">Status*</th>
+    <th class="letter_grade_header">Grade</th>
+    <th class="grade_points_header">Grade Points**</th>
+    <th class="transfer_credits_header">Transfer Credits**</th>
+    <th class="hes_credits_header">HES Credits**</th>
+</tr>
 
 <tr>
-<th class="semester_header">Semester</th>
-              <th class="course_number_header">Course Number</th>
-              <th class="course_delivery_header">Course Delivery</th>
-              <th class="crn_number_header">CRN#</th>
-              <th  class="section_header">Section</th>
-              <th class="tuition_header">Tuition</th>
-              <th class="status_header">Status*</th>
-              <th class="letter_grade_header">Grade</th>
-              <th class="grade_points_header">Grade Points**</th>
-              <th class="transfer_credits_header">Transfer Credits**</th>
-              <th class="hes_credits_header">HES Credits**</th>
-    </tr>
-    <tr>
-<td class="semester_box">' ?>  {{{$oneCourse->semester  }}}<?php echo '</td>
-<td class="course_number_box">' ?> {{{$oneCourse->course_number}}} <?php echo '</td>
-<td class="course_delivery_box">' ?>  {{{$oneCourse->course_delivery }}} <?php echo '</td>
-<td class="crn_number_box">' ?>  {{{$oneCourse->crn_number }}}<?php echo '</td>
-<td class="section_box">' ?>  {{{ $oneCourse->section  }}}<?php echo '</td>
-<td class="tuition_box">' ?>  {{{$oneCourse->tuition  }}}<?php echo '</td>
-<td class="status_box">' ?>  {{{$oneCourse->status  }}}<?php echo '</td>
-<td class="letter_grade_box">' ?>  {{{$oneCourse->letter_grade  }}}<?php echo '</td>
-<td class="grade_points_box">' ?>  {{{$oneCourse->grade_points  }}}<?php echo '</td>
-<td class="transfer_credits_box">' ?>  {{{$oneCourse->transfer_credits  }}}<?php echo '</td>
-<td class="hes_credits_box">' ?>  {{{ $oneCourse->hes_credits  }}}<?php echo '</td>
+    <td class="semester_box">' ?>  {{{$oneCourse->semester  }}}<?php echo '</td>
+    <td class="course_number_box">' ?> {{{$oneCourse->course_number}}} <?php echo '</td>
+    <td class="course_delivery_box">' ?>  {{{$oneCourse->course_delivery }}} <?php echo '</td>
+    <td class="crn_number_box">' ?>  {{{$oneCourse->crn_number }}}<?php echo '</td>
+    <td class="section_box">' ?>  {{{ $oneCourse->section  }}}<?php echo '</td>
+    <td class="tuition_box">' ?>  {{{$oneCourse->tuition  }}}<?php echo '</td>
+    <td class="status_box">' ?>  {{{$oneCourse->status  }}}<?php echo '</td>
+    <td class="letter_grade_box">' ?>  {{{$oneCourse->letter_grade  }}}<?php echo '</td>
+    <td class="grade_points_box">' ?>  {{{$oneCourse->grade_points  }}}<?php echo '</td>
+    <td class="transfer_credits_box">' ?>  {{{$oneCourse->transfer_credits  }}}<?php echo '</td>
+    <td class="hes_credits_box">' ?>  {{{ $oneCourse->hes_credits  }}}<?php echo '</td>
 </tr>
 
 <tr class="data_row">
     <td class = "edit_or_delete_rows" colspan="22">
-             <span>
-                    <a class="edit_or_delete_text" href="/course_edit/' ?> {{$oneCourse->id}}<?php echo '" > Edit course information</a>
-             </span>
-             <span>
-                    <a class="edit_or_delete_text" href="/delete/' ?> {{$oneCourse->id}}<?php echo '" > Delete course entry </a>
-             </span>
+      <span>
+          <a class="edit_or_delete_text" href="/course_edit/' ?> {{$oneCourse->id}}<?php echo '" > Edit course information</a>
+      </span>
+      <span>
+          <a class="edit_or_delete_text" href="/delete/' ?> {{$oneCourse->id}}<?php echo '" > Delete course entry </a>
+      </span>
     </td>
 </tr>
 <tr class = "separator_row">
@@ -96,17 +93,20 @@ echo '
  }
 
 ?>
-              @endforeach
+@endforeach
+
 <tr class = "bottom_separator_row">
     <td  colspan="22">
 </tr>
-        <?php
-    }
-    else {
-        echo '<p>No courses found<p>';
-    }
-    $total_credits = $total_hes_credits + $total_transfer_credits;
-   ?>
+<?php
+}
+else
+{
+ echo '<p>No courses found<p>';
+}
+$total_credits = $total_hes_credits + $total_transfer_credits;
+?>
+
 </table><br>
 <div class="font_wrapper">
 <div>Total credits: {{{$total_credits}}} credits.  You have {{{$total_hes_credits}}} HES and {{{$total_transfer_credits}}} transfer credits.</div>
@@ -132,7 +132,7 @@ echo " 0.00</div> ";
 Graduation Eligibility:
 <?php
 
- if($alm_or_alb === "Bachelor's of Liberal Arts (ALB)" && $total_credits >= 128 && $gpa >= 2.0)
+ if($alm_or_alb === "Bachelor's of Liberal Arts (ALB)" && $total_credits >= 128 && $gpa >= 2.0 && $total_hes_credits >= 64)
  {
 echo "Congratulations!  You are eligible to graduate.";
 }
@@ -185,6 +185,30 @@ echo "3.0 GPA.";
 }
 ?>
  You have a {{{ $gpa }}} GPA.</div><br>
+<?php
+
+ if($alm_or_alb === "Bachelor's of Liberal Arts (ALB)")
+ {
+     echo '<div>You need at least 64 HES credits.  You have ' + $total_hes_credits + '.</div>';
+     echo '<div>You need 40 credits in your Concentration, 32 must be HES credits.  You have ' + '.</div>';
+
+     echo '<div>You need 8 Sciences credits.  You have ' + '.</div>';
+     echo '<div>You need 8 Social Sciences credits.  You have ' + '.</div>';
+     echo '<div>You need 8 Humanities credits.  You have ' + '.</div>';
+     echo '<div>You need 8 credits outside the areas of concentration.  You have ' + '.</div>';
+
+     echo '<div>You need 8 Expository Writing credits.  You have ' + '.</div>';
+     echo '<div>You need 12 Writing Intensive credits.  You have ' + '.</div>';
+     echo '<div>You need either 8 lower-level Foreign Language credits or 4 upper-level.  You have ' + '.</div>';
+
+     echo '<div>You need 4 Quantitative Reasoning credits.  You have ' + '.</div>';
+     echo '<div>You need 4 Moral Reasoning credits.  You have ' + '.</div>';
+
+     echo '<div>You need 52 credits taught by Harvard Instructors.  You have ' + '.</div>';
+     echo '<div>You need 16 credits for the residency requirement.  You have ' + '.</div>';
+    echo '<div>You need 60 upper-level credits.  You have ' + '.</div>';
+ }
+?>
 
 </div>
 </div>
