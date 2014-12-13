bca4fb95331f1402b52488a3a0e6b35c8df3fc77
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-12-12 22:49:16 -0600

    css

--- app/views/_base_welcome.blade.php
+++ app/views/_base_welcome.blade.php
@@ -16,11 +16,14 @@
     	<div class='flash-message'>{{ Session::get('flash_message') }}</div>
     @endif
 
+<div class="padding_welcome">
     @if(Auth::check())
         <div class="login_logout_signup_welcome"><a href='/logout'>Log out {{ Auth::user()->email; }}</a></div>
     @else
         <div class="login_logout_signup_welcome"><a href='/signup'>Sign up</a> or <a href='/login'>Log in</a></div>
-    @endif
+
+   @endif
+
 <div class="font_wrapper_sign_in_log_in">
     <h1 class="welcome_header">Welcome to Degree Tracker<br>
     <div class="welcome_header_subtitle">A simple and fun tool for you and your school to organize your planned, current and completed courses</div> </h1>
@@ -31,6 +34,6 @@
     @yield('middle')
 
     @yield('bottom')
-
+</div>
 </body>
 </html>
